e54745bf20c4b284edfb0cf52d90f9574abf472c
vinayaksingh29 <vinayakpundhir29@gmail.com>
2025-01-19 01:47:54 +0530

    Made Changes in the Scheduler, now tasks can be executed based upon priority. If priority is same of tasks they will be executed on the basis of on what sequence they were added.

--- Scheduler.c
+++ Scheduler.c
@@ -1,66 +1,85 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX_TASKS UCHAR_MAX
 
 // Task states
-typedef enum {
+typedef enum 
+{
     TASK_READY,
     TASK_RUNNING,
     TASK_SUSPENDED
+
 } task_state_t;
 
 // Task Control Block structure
-typedef struct {
-    uint32_t *stack_pointer;  // Points to the task's stack (not used in this simple example)
-    uint8_t priority;         // Task priority (lower is higher priority)
-    task_state_t state;       // Current state of the task
-    void (*task_func)(void);  // Pointer to the task's main function
+typedef struct
+{
+    uint32_t *stack_pointer;    // Placeholder for stack pointer (not used here)
+    uint8_t priority;           // Priority of the task (lower value = higher priority)
+    task_state_t state;         // Current state of the task
+    void (*task_func)(void);    // Function pointer to the task's main function
+
 } tcb_t;
 
-#define MAX_TASKS 5
-
-tcb_t task_list[MAX_TASKS];  // Array to store tasks
-uint8_t current_task = 0;    // Tracks the current task index
+tcb_t task_list[MAX_TASKS];     // Array of task control blocks
+uint8_t task_index = NULL;         // Index of the next task to be added   
 
 // Initialize the scheduler
-void init_scheduler() {
-    for (int i = 0; i < MAX_TASKS; i++) {
-        task_list[i].state = TASK_SUSPENDED;  // Initially, all tasks are inactive
+void init_scheduler() 
+{
+    for (int i = NULL; i < MAX_TASKS; i++) 
+    {
+        task_list[i].state = TASK_SUSPENDED;    // Set all tasks to suspended initially
     }
+
 }
 
 // Add a task to the scheduler
-void add_task(void (*task_func)(void), uint8_t priority) {
-    static uint8_t task_index = 0;
-    if (task_index < MAX_TASKS) {
+void add_task(void (*task_func)(void), uint8_t priority) 
+{
+    if (task_index < MAX_TASKS) 
+    {
         task_list[task_index].task_func = task_func;
         task_list[task_index].priority = priority;
         task_list[task_index].state = TASK_READY;
+
         task_index++;
-    } else {
+
+        for (int index_val = NULL; index_val < task_index - 1; index_val++)    // Sort tasks based on priority after each addition : Bubble sort
+        {
+            for (int sort_val = index_val + 1; sort_val < task_index; sort_val++) 
+            {
+                if (task_list[index_val].priority > task_list[sort_val].priority) 
+                {
+                    tcb_t temp = task_list[index_val];                      // Swap the tasks
+                    task_list[index_val] = task_list[sort_val];
+                    task_list[sort_val] = temp;
+                }
+            }
+        }
+    } 
+    
+    else 
+    {
         printf("Task list is full!\n");
     }
 }
 
-// Start the scheduler
-void start_scheduler() {
-    while (1) {
-        int highest_priority_task = -1;
-        uint8_t highest_priority = 255; // Start with the lowest possible priority value
-
-        // Find the task with the highest priority that is ready
-        for (int i = 0; i < MAX_TASKS; i++) {
-            if (task_list[i].state == TASK_READY && task_list[i].priority < highest_priority) {
-                highest_priority = task_list[i].priority;
-                highest_priority_task = i;
+void start_scheduler() 
+{
+    while (1) 
+    {
+        // Find the next ready task with the highest priority (lowest priority value)
+        for (int task_val = NULL; task_val < task_index; task_val++) 
+        {
+            if (task_list[task_val].state == TASK_READY) 
+            {
+                task_list[task_val].state = TASK_RUNNING;
+                task_list[task_val].task_func();
+                task_list[task_val].state = TASK_READY; // Set task back to ready after execution
             }
-        }
-
-        // If a ready task is found, execute it
-        if (highest_priority_task != -1) {
-            current_task = highest_priority_task;
-            task_list[current_task].state = TASK_RUNNING;
-            task_list[current_task].task_func(); // Run the task
-            task_list[current_task].state = TASK_READY; // Set back to ready after execution
         }
     }
 }
@@ -80,9 +99,9 @@ void task3() {
 
 int main() {
     init_scheduler();
-    add_task(task1, 3); // Task 1 has priority 3
-    add_task(task2, 1); // Task 2 has priority 1 (highest priority)
-    add_task(task3, 2); // Task 3 has priority 2
+    add_task(task3, 2);  // Task with priority 2
+    add_task(task1, 3);  // Task with priority 3
+    add_task(task2, 1);  // Task with priority 1
 
     printf("Starting scheduler...\n");
     start_scheduler();
